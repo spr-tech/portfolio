0e4888a139f9fa77b452add5b8713093f31c221c
spr-tech <adetoroshina434@gmail.com>
2026-03-31 22:42:52 +0100

    updated some of my code to be more cleaner and readable

--- src/components/EducationAndSkills.jsx
+++ src/components/EducationAndSkills.jsx
@@ -1,84 +1,103 @@
 import "./EducationAndSkills.css";
 
 const EducationAndSkills = () => {
+  const skillsData = [
+    {
+      title: "Programming Languages",
+      items: ["TypeScript", "JavaScript"],
+    },
+    {
+      title: "Frameworks & Libraries",
+      items: ["React", "Bootstrap", "Tailwind", "Next.js", "Vite"],
+    },
+    {
+      title: "Software & Tools",
+      items: ["Jira", "GitHub"],
+    },
+    {
+      title: "Methodologies",
+      items: ["Code Review", "Remote Collaboration"],
+    },
+    {
+      title: "Operating Systems",
+      items: ["Mac/iOS", "Windows"],
+    },
+  ];
+
+  const softSkillsData = [
+    {
+      title: "Leadership & Mentorship",
+      items: [
+        "Conflict Resolution",
+        "Code Review",
+        "Training and Development",
+        "Mentoring",
+        "Empathy and Patience",
+      ],
+    },
+    {
+      title: "Execution & Strategy",
+      items: [
+        "Practical Problem Solving",
+        "Prioritization",
+        "Iterative Development",
+        "User-Focused Thinking",
+        "Resourcefulness",
+      ],
+    },
+    {
+      title: "Adaptability",
+      items: [
+        "Flexibility",
+        "Growth Mindset",
+        "User-Focused Thinking",
+        "Remote Team Collaboration",
+      ],
+    },
+  ];
+
   return (
     <div>
-      <div className="education">
+      {/* Education */}
+      <section className="education">
         <h4>Education</h4>
-        <hr></hr>
+        <hr />
         <p>
           Bachelor of Science in Education (B.Sc. Ed.) in Business Education
         </p>
         <i>Oct 2023</i>
         <span>University of Ilorin, Kwara State, Nigeria</span>
-      </div>
+      </section>
 
-      <div className="skills">
+      {/* Technical Skills */}
+      <section className="skills">
         <h4>Technical Skills</h4>
-        <hr></hr>
-        <div className="skill-1">
-          <h5>Programming Languages</h5>
-          <span>TypeScript</span>
-          <span>JavaScript</span>
-        </div>
+        <hr />
 
-        <div className="other-skill">
-          <h5>Frameworks & Libraries</h5>
-          <span>React</span>
-          <span>Bootstap</span>
-          <span>Tailwind</span>
-          <span>Next.js</span>
-          <span>Vite</span>
-        </div>
+        {skillsData.map((skill, index) => (
+          <div className="other-skill" key={index}>
+            <h5>{skill.title}</h5>
+            {skill.items.map((item, i) => (
+              <span key={i}>{item}</span>
+            ))}
+          </div>
+        ))}
+      </section>
 
-        <div className="other-skill">
-          <h5>Software & Tools</h5>
-          <span>Jira</span>
-          <span>Github</span>
-        </div>
+      {/* Soft Skills */}
+      <section className="skills">
+        <h4>Soft Skills</h4>
+        <hr />
 
-        <div className="other-skill">
-          <h5>Methodologies</h5>
-          <span>Code Review</span>
-          <span>Remote Collaboration</span>
-        </div>
-
-        <div className="other-skill">
-          <h5>Operating Systems</h5>
-          <span>Mac/iOS</span>
-          <span>Windows</span>
-        </div>
-      </div>
-
-      <h4>Soft skills</h4>
-      <hr />
-      <div className="skills">
-        <div className="other-skill">
-          <h5>Leadership & Mentorship</h5>
-          <span>Conflict Resolution</span>
-          <span>Code Review</span>
-          <span>Training and Development</span>
-          <span>Mentoring</span>
-          <span>Empathy and Patience</span>
-        </div>
-
-        <div className="other-skill">
-          <h5>Execution & Strategy</h5>
-          <span>Practical Problem Solving</span>
-          <span>Prioritization</span>
-          <span>Iterative Development </span>
-          <span>User-Focused Thinking</span>
-          <span>Resourcefulness</span>
-        </div>
-
-        <div className="other-skill">
-          <h5>Adaptability</h5>
-          <span>Flexibility</span>
-          <span>Growth Mindset</span>
-          <span>User-Focused Thinking</span>
-          <span>Remote Team Collaboration</span>
-        </div>
-      </div>
+        {softSkillsData.map((skill, index) => (
+          <div className="other-skill" key={index}>
+            <h5>{skill.title}</h5>
+            {skill.items.map((item, i) => (
+              <span key={i}>{item}</span>
+            ))}
+          </div>
+        ))}
+      </section>
     </div>
   );
 };
